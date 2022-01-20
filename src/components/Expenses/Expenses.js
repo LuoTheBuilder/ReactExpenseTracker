0833399cb2ef8bc0d313cfe243dad27b94ac246c
LuoTheBuilder <rawbailey@outlook.com>
2022-01-20 13:54:25 -0500

    Added add expense functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,29 +1,26 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpenseDate from "./ExpenseDate";
 
 const Expenses = (Props) => {
+  // adding a filter here
+  const saveFilterExpenses = (event) => {
+    const expenses = { event, id: Math.random().toString() };
+    console.log(expenses);
+  };
+
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={Props.expenses[0].title}
-        amount={Props.expenses[0].amount}
-        date={Props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={Props.expenses[1].title}
-        amount={Props.expenses[1].amount}
-        date={Props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={Props.expenses[2].title}
-        amount={Props.expenses[2].amount}
-        date={Props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={Props.expenses[3].title}
-        amount={Props.expenses[3].amount}
-        date={Props.expenses[3].date}
-      />
+      <ExpensesFilter onFilterExpenses={saveFilterExpenses} />
+      {Props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 };
